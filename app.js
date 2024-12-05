ec3dabcb5f24045e1f6ed3b285477ba14a6bcc94
Giacomo Maldarella <giacomomaldarella@gmail.com>
2024-12-05 13:24:27 +0100

    gacharoll frontend

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://localhost:5001/auth_service";
 const BASE_URL_PAYMENT = "https://localhost:5001/payment_service";
 const BASE_URL_AUCTION = "https://localhost:5001/auction_service";
-
+const BASE_URL_GACHA = "https://localhost:5001/gacha_roll";
 
 const CERT_OPTIONS = { rejectUnauthorized: false }; // Gestione certificati autofirmati
 
@@ -336,6 +336,54 @@ document.getElementById("bid-auction-button").addEventListener("click", async ()
       document.getElementById("service-result").textContent = JSON.stringify(data);
     } else {
       alert(data.error || "Failed to place bid");
+    }
+  } catch (error) {
+    alert("Error: " + error.message);
+  }
+});
+
+
+// Gacha Roll
+document.getElementById("gacha-roll-button").addEventListener("click", async () => {
+  const accessToken = localStorage.getItem("access_token");
+  const username = localStorage.getItem("logged_username");
+  const level = prompt("Enter roll level (standard, medium, premium):");
+
+  if (!username || !level) {
+    alert("Username or roll level is missing!");
+    return;
+  }
+
+  const resultSection = document.getElementById("gacha-roll-result");
+  const gachaImage = document.getElementById("gacha-roll-image");
+  const gachaDetails = document.getElementById("gacha-roll-details");
+
+  try {
+    // Hide previous result and show loading state
+    resultSection.style.display = "none";
+
+    const response = await fetch(`${BASE_URL_GACHA}/gacharoll`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ username, level }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      // Update UI with Gacha result
+      gachaImage.src = data.img;
+      gachaDetails.innerHTML = `
+        <strong>Name:</strong> ${data.gacha_name}<br>
+        <strong>Rarity:</strong> ${data.rarity}<br>
+        <strong>Description:</strong> ${data.description}
+      `;
+      resultSection.style.display = "block";
+    } else {
+      alert(data.Error || "Gacha Roll Failed");
     }
   } catch (error) {
     alert("Error: " + error.message);
